00217fc7a1853d41a217bb07e7470aedaf687235
the-paranoid07 <121954633+the-paranoid07@users.noreply.github.com>
2023-06-22 12:18:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function getFormvalue() {
+    function getFormvalue() {
     //Write your code here
+	
+		this.alert("hello");
+	const form=document.getElementById("form1");
+
+	form.addEventListener("submit",(event) => {
+		
+		const formElement = event.target;
+		let firstName=formElement["fname"].value.trim(),
+		let lastName=formElement["lname"].value.trim(),
+			
+	})
+}
 
 }
